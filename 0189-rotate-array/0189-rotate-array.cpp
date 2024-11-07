ad475fbdbce2d93a60468940e64dc56d36b6a0ac
U_Aadarsh <106325783+2-0aadarsh@users.noreply.github.com>
2024-11-08 01:44:04 +0530

    Time: 0 ms (100%), Space: 27.4 MB (92.06%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        k = k%n;
-        vector<int> temp;
+        // int n = nums.size();
+        // k = k%n;
+        // vector<int> temp;
 
-        for(int i=(n-k); i<n; i++){
-            temp.push_back(nums[i]);
-        }
+        // for(int i=(n-k); i<n; i++){
+        //     temp.push_back(nums[i]);
+        // }
 
-        for(int i=0; i<=(n-k-1); i++) {
-            temp.push_back(nums[i]);
-        }
+        // for(int i=0; i<=(n-k-1); i++) {
+        //     temp.push_back(nums[i]);
+        // }
 
-        for(int i=0; i<temp.size(); i++){
-            nums[i] = temp[i];
-        }
+        // for(int i=0; i<temp.size(); i++){
+        //     nums[i] = temp[i];
+        // }
+
+        // method-2 in O(1) space
+         int n = nums.size();
+        k = k % n;  // Handle cases where k is greater than n
+        
+        // Step 1: Reverse the entire array
+        reverse(nums.begin(), nums.end());
+        
+        // Step 2: Reverse the first k elements
+        reverse(nums.begin(), nums.begin() + k);
+        
+        // Step 3: Reverse the remaining n - k elements
+        reverse(nums.begin() + k, nums.end());
+
     }
 };
